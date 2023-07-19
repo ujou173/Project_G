17462164f90147d07378369bb38accd7d81811ab
Rosumin <ujou173@gmail.com>
2023-07-20 01:43:41 +0900

    배경 설정 및 camera를 이용해 캐릭터를 항상 센터에 잡도록 설정 / 저작권을 생각해 임시 캐릭터를 사각형으로 변경

--- game.js
+++ game.js
@@ -9,33 +9,47 @@ var config = {
   scene: {
       preload: preload,
       create: create,
-      update: update
+      update: update,
   }
 };
 
 const game = new Phaser.Game(config);
 let player;
+let cursors;
 
 function preload() {
-  this.load.image('player', 'assets/player.png');
+    this.load.image('background', 'assets/bg.png');
+    this.load.image('player', 'assets/player.png');
 }
 
 function create() {
-  player = this.add.sprite(1000, 700, 'player');
+    let background = this.add.sprite(0, 0, 'background');
+
+    // 배경 스프라이트 위치 설정
+    background.setOrigin(0, 0);
+    background.setScale(1);
+
+    // 게임 요소를 배경보다 앞으로 가져옴
+    this.children.bringToTop(background);
+
+    player = this.add.sprite(this.cameras.main.width / 2, this.cameras.main.height / 2, 'player');
+
+    cursors = this.input.keyboard.createCursorKeys();
+
+    this.cameras.main.setBounds(0, 0, '100%', '100%');
+    this.cameras.main.startFollow(player, true, 0.5, 0.5);
 }
 
 function update() {
-  const cursors = this.input.keyboard.createCursorKeys();
+    if (cursors.up.isDown) {
+        player.y -= 5;
+    } else if (cursors.down.isDown) {
+        player.y += 5;
+    }
 
-  if (cursors.up.isDown) {
-      player.y -= 5;
-  } else if (cursors.down.isDown) {
-      player.y += 5;
-  }
-
-  if (cursors.left.isDown) {
-      player.x -= 5;
-  } else if (cursors.right.isDown) {
-      player.x += 5;
-  }
+    if (cursors.left.isDown) {
+        player.x -= 5;
+    } else if (cursors.right.isDown) {
+        player.x += 5;
+    }
 }
