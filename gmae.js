663c1bd6f4d289fa03ce7bff60eefdd51d67431d
Rosumin <ujou173@gmail.com>
2023-07-20 00:32:06 +0900

    프로젝트 시작 / DI 설치 / 임시 플레이어 이미지 추가 / 상하좌우로 이동시키는 로직 추가

---
+++ gmae.js
@@ -0,0 +1,37 @@
+var config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  scene: {
+      preload: preload,
+      create: create,
+      update: update
+  }
+};
+
+var game = new Phaser.Game(config);
+var player;
+
+function preload() {
+  this.load.image('player', 'assets/player.png');
+}
+
+function create() {
+  player = this.add.sprite(400, 300, 'player');
+}
+
+function update() {
+  var cursors = this.input.keyboard.createCursorKeys();
+
+  if (cursors.up.isDown) {
+      player.y -= 5;
+  } else if (cursors.down.isDown) {
+      player.y += 5;
+  }
+
+  if (cursors.left.isDown) {
+      player.x -= 5;
+  } else if (cursors.right.isDown) {
+      player.x += 5;
+  }
+}
