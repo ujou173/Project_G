09e1638b99b8c42e423a3460d37b6c3980d9b449
Rosumin <ujou173@gmail.com>
2023-07-21 16:00:30 +0900

    캐릭터 이동 방식을 좌표 변경 방식에서 물리엔진 시뮬레이션을 이용하도록 변경중 / 캐릭터가 사라짐 or 움직임이 적용되지 않는 문제 / 물리엔진 적용하는 방법에 대해 알아볼 필요가 있음

--- game.js
+++ game.js
@@ -3,8 +3,8 @@ var config = {
   width: '100%',
   height: '100%',
   scale: {
-    mode: Phaser.Scale.FIT,
-    autoCenter: Phaser.Scale.CENTER_BOTH
+  mode: Phaser.Scale.FIT,
+  autoCenter: Phaser.Scale.CENTER_BOTH
   },
   scene: {
       preload: preload,
@@ -18,39 +18,62 @@ let player;
 let cursors;
 
 function preload() {
-    this.load.image('background', 'assets/bg.png');
-    this.load.image('player', 'assets/player.png');
+  this.load.image('background', 'assets/bg.png');
+  this.load.image('player', 'assets/player.png');
 }
 
 function create() {
-    let background = this.add.sprite(0, 0, 'background');
+  // 맵 크기 설정
+  const mapWidth = 2000;
+  const mapHeight = 1600;
 
-    // 배경 스프라이트 위치 설정
-    background.setOrigin(0, 0);
-    background.setScale(1);
+  let background = this.add.sprite(this.cameras.main.width / 2, this.cameras.main.height / 2, 'background');
+  background.displayWidth = this.cameras.main.width;
+  background.displayHeight = this.cameras.main.height;
+  background.setOrigin(0.5);
 
-    // 게임 요소를 배경보다 앞으로 가져옴
-    this.children.bringToTop(background);
+  // 게임 요소를 배경보다 앞으로 가져옴
+  this.children.bringToTop(background);
 
-    player = this.add.sprite(this.cameras.main.width / 2, this.cameras.main.height / 2, 'player');
-    player.setScale(0.5);
+  player = this.physics.add.sprite(this.cameras.main.width / 2, this.cameras.main.height / 2, 'player');
+  player.setScale(0.5);
+  player.setCollideWorldBounds(true); // 맵 경계와 충돌하도록 설정
 
-    cursors = this.input.keyboard.createCursorKeys();
+  cursors = this.input.keyboard.createCursorKeys();
 
-    this.cameras.main.setBounds(0, 0, '100%', '100%');
-    this.cameras.main.startFollow(player, true, 0.5, 0.5);
+  this.cameras.main.setBounds(0, 0, '100%', '100%');
+  this.cameras.main.startFollow(player, true, 0.5, 0.5);
+
+  // 캐릭터의 초기 위치 설정 (화면 중앙)
+  player.x = this.cameras.main.width / 2;
+  player.y = this.cameras.main.height / 2;
 }
 
 function update() {
-    if (cursors.up.isDown) {
-        player.y -= 5;
-    } else if (cursors.down.isDown) {
-        player.y += 5;
-    }
+  if (cursors.up.isDown) {
+    player.setVelocityY(-160); // setVelocityY() => 물리엔진이 적용된 움직임. 플레이어를 Y축을 기준으로 160의 속도로 -방향으로 움직인다는 뜻
+  } else if (cursors.down.isDown) {
+    player.setVelocityY(160);
+  } else {
+    player.setVelocityY(0);
+  }
 
-    if (cursors.left.isDown) {
-        player.x -= 5;
-    } else if (cursors.right.isDown) {
-        player.x += 5;
-    }
+  if (cursors.left.isDown) {
+    player.setVelocityX(-160);
+  } else if (cursors.right.isDown) {
+    player.setVelocityX(160);
+  } else {
+    player.setVelocityX(0);
+  }
+
+  // 맵의 크기 제한 설정
+  const halfPlayerWidth = player.displayWidth / 2;
+  const halfPlayerHeight = player.displayHeight / 2;
+  const minX = halfPlayerWidth;
+  const maxX = mapWidth - halfPlayerWidth;
+  const minY = halfPlayerHeight;
+  const maxY = mapHeight - halfPlayerHeight;
+  
+  player.x = Phaser.Math.Clamp(player.x, minX, maxX);
+  player.y = Phaser.Math.Clamp(player.y, minY, maxY);
 }
